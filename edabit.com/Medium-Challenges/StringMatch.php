da557cb6c2bf87f0aa551daf06cdf4261350826b
davidlomath <davidlomath@hotmail.co.uk>
2022-01-18 17:13:51 +0000

    Create a function that takes two strings, $a and $b. Return the number of times the two strings contain the same two letters at the same index. The two letters must appear at consecutive indices.

---
+++ edabit.com/Medium-Challenges/StringMatch.php
@@ -0,0 +1,37 @@
+<?php
+// https://edabit.com/challenge/3QXHLxo9W63r6thLa
+
+class StringMatch
+{
+    public function match($word1, $word2): int {
+        $array1 = str_split($word1);
+        $array2 = str_split($word2);
+
+        if (count($array1) > count($array2)) {
+            $needle = $array2;
+            $haystack = $array1;
+        } else {
+            $needle = $array1;
+            $haystack = $array2;
+        }
+
+        $match = 0;
+        for ($i = 0; $i < count($needle); $i++) {
+            if (isset($needle[$i + 1])) {
+                $tneedle = $needle[$i].$needle[$i+1];
+                $thaystack = $haystack[$i].$haystack[$i+1];
+
+                if ($tneedle === $thaystack) {
+                    $match++;
+                }
+            }
+        }
+        return $match;
+    }
+}
+
+$str = new StringMatch();
+
+echo $str->match("yytaazz", "yyjaaz"); // 3
+echo $str->match("edabit", "ed"); // 1
+echo $str->match("", ""); // 0
